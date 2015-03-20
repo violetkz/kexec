6394c6fd84abb84bad664cfa2fd1a3820ba0fdb4
ken <proxy.vk@gmail.com>
2015-03-20 17:41:58 +0800

    modify trace funcion

--- trace.c
+++ trace.c
@@ -7,19 +7,26 @@
 #include <stdarg.h>
 
 
-static int fd_log_desc = STDERR_FILENO;
+//static int fd_log_desc = STDERR_FILENO;
+static FILE* fd_log_desc = NULL;
 
 void log_set_desc(FILE* fp) {
-    //fd_log_desc = fp;
+    fd_log_desc = fp;
+}
+
+static void vlog(const char* prefix, char* fmt, va_list ap) {
+    FILE* fd_log =  (fd_log_desc != NULL) ? fd_log_desc : stderr;
+        
+    fprintf(fd_log, prefix);
+    vfprintf(fd_log, fmt, ap);
+    fprintf(fd_log, "\n");
 }
 
 void dlog(char *fmt, ...) {
-    
-    fprintf(stderr, "[DEBUG]");
-
+#ifdef __debug__ 
     va_list ap;    
     va_start(ap, fmt);
-    //vfprintf(fd_log_desc, fmt, ap);
-    vfprintf(stderr, fmt, ap);
+    vlog("-D-", fmt, ap);
     va_end(ap);
+#endif
 }
